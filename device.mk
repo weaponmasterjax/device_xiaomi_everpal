ded6d0e24a8e37ee2c06233e04a9a1cfbd43bdca
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-21 01:58:57 +0300

    everpal: sensors: Avoid target name conflicts
    Change-Id: I170c8c7e4edbfcdebf5d6f6df148b72993d805f6

--- device.mk
+++ device.mk
@@ -417,7 +417,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.everpal-multihal \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
