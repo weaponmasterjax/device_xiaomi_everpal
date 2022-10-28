4d3a904390eff66fca26cdc5cc4fc76ec9259bbd
John Stultz <john.stultz@linaro.org>
2022-10-28 19:40:54 +0100

    everpal: Disable nonblocking_ffs to avoid recent crashes with adbd
    Recent changes to adbd have enabled nonblocking_ffs by default.
    
    These have seeminly uncovered some bad memory corruption in the
    kernel ffs gadget driver on every kernel I've tried so far.
    
    Unfortunately I've not been able to diagnose what is going wrong,
    so for now, lets disable the nonblocking_ffs.
    
    We still need to root cause the issue as it's a problem even with
    the mainline kernel on HiKey, so its likely a generic kernel issue.
    
    Disable nonblocking_ffs in recovery, too.
    
    Change-Id: I7a0797cc9e8e5f263e533beea23642f768c0f017
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- device.mk
+++ device.mk
@@ -451,6 +451,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-mediatekv2
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.adb.nonblocking_ffs=0
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.everpal
