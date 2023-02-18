a5c1f46d3c817e8e0f1e69b96a4a9e186f621823
Taqi Raza <mttalpur@gmail.com>
2023-02-18 18:49:03 +0500

    everpal: move system|vendor props under configs/props

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,8 +152,8 @@ TARGET_BOARD_PLATFORM := mt6833
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
 # Recovery
 BOARD_USES_RECOVERY_AS_BOOT := true
