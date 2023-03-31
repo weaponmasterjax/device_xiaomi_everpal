3db7032c51a0fd35c92476598d8a1439096f257c
Taqi Raza <mttalpur@gmail.com>
2023-03-31 18:06:15 +0500

    everpal: add multiple os build support
    everpal: add lineageos and arrow build support
    
    
    everpal: add sparkos build support
    
    
    everpal: add evolutionx build support
    
    
    everpal: add cherishos build support
    
    
    everpal: add octavi build support
    
    
    everpal: add derp build support
    
    
    everpal: add leaf build support
    
    
    everpal: add superior build support
    
    
    everpal: add banana build support
    
    
    everpal: add havoc build support

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,53 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_everpal.mk
+    $(LOCAL_DIR)/aosp_everpal.mk \
+    $(LOCAL_DIR)/arrow_everpal.mk \
+    $(LOCAL_DIR)/banana_everpal.mk \
+    $(LOCAL_DIR)/cherish_everpal.mk \
+    $(LOCAL_DIR)/derp_everpal.mk \
+    $(LOCAL_DIR)/evolution_everpal.mk \
+    $(LOCAL_DIR)/havoc_everpal.mk \
+    $(LOCAL_DIR)/leaf_everpal.mk \
+    $(LOCAL_DIR)/lineage_everpal.mk \
+    $(LOCAL_DIR)/octavi_everpal.mk \
+    $(LOCAL_DIR)/spark_everpal.mk \
+    $(LOCAL_DIR)/superior_everpal.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_everpal-user \
     aosp_everpal-userdebug \
-    aosp_everpal-eng
+    aosp_everpal-eng \
+    arrow_everpal-user \
+    arrow_everpal-userdebug \
+    arrow_everpal-eng \
+    banana_everpal-user \
+    banana_everpal-userdebug \
+    banana_everpal-eng \
+    cherish_everpal-user \
+    cherish_everpal-userdebug \
+    cherish_everpal-eng \
+    derp_everpal-user \
+    derp_everpal-userdebug \
+    derp_everpal-eng \
+    evolution_everpal-user \
+    evolution_everpal-userdebug \
+    evolution_everpal-eng \
+    havoc_everpal-user \
+    havoc_everpal-userdebug \
+    havoc_everpal-eng \
+    leaf_everpal-user \
+    leaf_everpal-userdebug \
+    leaf_everpal-eng \
+    lineage_everpal-user \
+    lineage_everpal-userdebug \
+    lineage_everpal-eng \
+    octavi_everpal-user \
+    octavi_everpal-userdebug \
+    octavi_everpal-eng \
+    spark_everpal-user \
+    spark_everpal-userdebug \
+    spark_everpal-eng \
+    superior_everpal-user \
+    superior_everpal-userdebug \
+    superior_everpal-eng
