14be2f71e1e38e7c85bc55615220d5649772d653
anant-goel <100245626+anant-goel@users.noreply.github.com>
2023-04-24 20:27:36 +0530

    everpal:  Set ro.product.mod_device correctly and label sepolicy for it
    * Proper set ro.product.mod_device depending on variant
    * This needed for MIUICamera to work
    
    Co-Authored-By: anant-goel <100245626+anant-goel@users.noreply.github.com>

--- libinit/include/libinit_variant.h
+++ libinit/include/libinit_variant.h
@@ -15,6 +15,7 @@ typedef struct variant_info {
     std::string sku_value;
 
     std::string brand;
+    std::string mod_device;
     std::string device;
     std::string name;
     std::string marketname;
