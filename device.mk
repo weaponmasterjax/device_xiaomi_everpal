ec690be7266d0234c3b9d399836f45546ad9c36c
weaponmasterjax <sd369888@outlook.com>
2023-08-08 16:16:30 +0800

    everpal: enforce debugfs restrictions

--- device.mk
+++ device.mk
@@ -145,6 +145,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.cas@1.2-service-lazy
 
+# DebugFS
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
