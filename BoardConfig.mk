1884997f2a9bbc68eaecaeb25463d4869eb01be0
weaponmasterjax <sd369888@outlook.com>
2023-08-09 11:08:01 +0800

    everpal: sepolicy: fix sensor hal denials
    everpal: sepolicy: address flag_health_check denials
    
    
    everpal: sepolicy: address hal_health_default denials
    
    
    everpal: sepolicy: initial private sepolicy
    
    
    everpal: sepolicy: sort out perfmgr denials
    
    
    everpal: sepolicy: allow system server to contact with init

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,6 +166,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
