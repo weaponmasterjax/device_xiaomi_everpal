b7fa3ded28ec7b468e29f708141be74b566998fb
rio004 <riyon336@gmail.com>
2023-08-10 09:55:20 +0000

    everpal: sepolicy: Address some camera and fingerprint denials

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,6 +163,8 @@ TARGET_USERIMAGES_USE_F2FS := true
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
