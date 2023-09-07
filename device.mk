5d5a301f77cf625224aafbafa0332b350cc5a36b
Taqi Raza <mttalpur@gmail.com>
2023-09-07 15:28:44 +0500

    everpal: device: remove ximi mlipay

--- device.mk
+++ device.mk
@@ -268,10 +268,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-# Mlipay
-PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.mlipay@1.1.vendor
-
 # Neural Networks
 PRODUCT_PACKAGES += \
     libruy \
