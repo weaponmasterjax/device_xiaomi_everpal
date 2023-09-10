14f6fd276c51774ec68cc5f12a84fc2713571001
rio004 <riyon336@gmail.com>
2023-09-10 16:06:44 +0500

    everpal: Kang powerhint from MT6768
     Adapt to MT6833
    
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -340,6 +340,9 @@ PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub \
     vendor.mediatek.hardware.mtkpower@1.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
