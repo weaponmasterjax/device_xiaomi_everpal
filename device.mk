a30645a99264d05a9e646c41ebd3dbf1b1bacaab
bengris32 <bengris32@protonmail.ch>
2023-09-16 01:08:50 +0500

    everpal: Ship MediaTek cgroup/task_profiles configuration
    * MediaTek ship their own modified task_profiles and
      use the V30 cgroups configuration.
    
    Change-Id: I15245ea87b00925597db65f1564e808a094c937a
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -134,6 +134,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,system/ca-certificates/files/,$(TARGET_COPY_OUT_VENDOR)/etc/security/cacerts)
 
+# Cgroup
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
