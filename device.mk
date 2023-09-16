1556fea2c8dd5e34096429d0ba3fe6f84c45297d
bengris32 <bengris32@protonmail.ch>
2023-09-16 20:54:14 +0500

    everpal: Switch to prebuilt libwifi-hal
    * Reference: https://github.com/moto-common/android_device_mediatek_common/tree/master/vendor/wlan
    
    * Perhaps it will help with the Wi-Fi scanning issues,
      and intermittent disconnections we're having. Besides,
      we can't legally use the OSS one anyway.
    
    Change-Id: I9bbf289ad88a12bbdc68ab0abdee410678bf1420
    Co-authored-by: electimon <electimon@gmail.com>
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -468,7 +468,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service-lazy \
     hostapd \
-    libwifi-hal-mt66xx \
     wpa_supplicant
 
 PRODUCT_PACKAGES += \
@@ -476,6 +475,4 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.control@1.1.vendor:64
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
