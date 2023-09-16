ed3f0b015008ef058b5a349b51627a85f9428e5f
weaponmasterjax <sd369888@outlook.com>
2023-09-16 21:10:48 +0800

    everpal: cleanup makefiles

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,17 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_everpal.mk \
-    $(LOCAL_DIR)/arrow_everpal.mk \
-    $(LOCAL_DIR)/lineage_everpal.mk
+    $(LOCAL_DIR)/evolution_everpal.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_everpal-user \
-    aosp_everpal-userdebug \
-    aosp_everpal-eng \
-    arrow_everpal-user \
-    arrow_everpal-userdebug \
-    arrow_everpal-eng \
-    lineage_everpal-user \
-    lineage_everpal-userdebug \
-    lineage_everpal-eng
+    evolution_everpal-user \
+    evolution_everpal-userdebug \
+    evolution_everpal-eng
