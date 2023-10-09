eac234efbbaafb16f5bf26f8d21e346fe8907bcb
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2023-10-09 23:58:44 +0800

    Revert "everpal: add RemovePackages"
    This reverts commit 6a6ef3e0dffcf8332229d09df212339161f271fd.

--- device.mk
+++ device.mk
@@ -474,10 +474,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
-# Remove Packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-mediatekv2
