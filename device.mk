42a4aceee7e072ef3bac15d06c40f99b22b89eaa
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-16 22:45:12 +0500

    everpal: Switch to Wifi service AIDL
    Change-Id: I5a2864b4159b7815eff4dd2f0de02a3dabbacfa5

--- device.mk
+++ device.mk
@@ -470,7 +470,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service-lazy \
+    android.hardware.wifi-service-lazy \
     hostapd \
     wpa_supplicant
 
