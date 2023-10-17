bd2b6fdd84a16ccded01cc5074cb8850ab26faa5
Taqi Raza <mttalpur@gmail.com>
2023-10-17 19:16:47 +0500

    everpal: downgrade audio hal impl version to 7.0

--- device.mk
+++ device.mk
@@ -60,7 +60,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@7.1-impl
+    android.hardware.audio@7.0-impl
 
 PRODUCT_PACKAGES += \
     android.hardware.audio.common-util.vendor \
