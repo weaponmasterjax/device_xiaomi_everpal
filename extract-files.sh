44971e7f0f898c06fa1092f46d5973f58a9a877f
Taqi Raza <mttalpur@gmail.com>
2023-10-18 15:11:27 +0500

    everpal: extract-files: fixup patchelf blobs

--- extract-files.sh
+++ extract-files.sh
@@ -56,28 +56,32 @@ fi
 # Patch/fix blobs
 function blob_fixup {
     case "$1" in
-    vendor/lib*/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
+    lib64/libsink.so)
+        grep -q "libshim_vtservice.so" "${2}" && \
+        "${PATCHELF}" --add-needed "libshim_vtservice.so" "${2}"
+        ;;
+    vendor/lib*/hw/vendor.mediatek.hardware.pq@*-impl.so)
+        grep -q "libutils.so" "${2}" && \
         "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
         ;;
     vendor/bin/hw/android.hardware.media.c2@1.2-mediatek*)
-       "$PATCHELF" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "$2"
+        grep -q "libavservices_minijail_vendor.so" "${2}" && \
+        "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
         ;;
     vendor/lib*/libmtkcam_stdutils.so)
-        "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
+        grep -q "libutils.so" "${2}" && \
+        "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
         ;;
-    vendor/bin/hw/android.hardware.gnss-service.mediatek)
-        ;&
-    vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
-       "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
+    vendor/bin/hw/android.hardware.gnss-service.mediatek | vendor/lib*/hw/android.hardware.gnss-impl-mediatek.so)
+        grep -q "android.hardware.gnss-V1-ndk_platform.so" "${2}" && \
+        "${PATCHELF}" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
         ;;
     vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc)
-        sed -i 's/start/enable/' "$2"
-		;;
-    vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
-        grep -q "libcamera_metadata_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
+        sed -i 's/start/enable/' "${2}"
         ;;
-    lib64/libsink.so)
-        "${PATCHELF}" --add-needed "libshim_vtservice.so" "${2}"
+    vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
+        grep -q "libcamera_metadata_shim.so" "${2}" && \
+        "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
         ;;
     vendor/lib*/libwvhidl.so | vendor/lib*/mediadrm/libwvdrmengine.so)
         grep -q "libprotobuf-cpp-lite-3.9.1.so" "${2}" && \
