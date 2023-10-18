32c9bc3ad18e62b6446faacfded312f8afd8dc4d
Taqi Raza <mttalpur@gmail.com>
2023-10-18 23:41:07 +0500

    everpal: update arrow build product

--- arrow_everpal.mk
+++ arrow_everpal.mk
@@ -9,17 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from everpal device
 $(call inherit-product, device/xiaomi/everpal/device.mk)
 
 # ArrowOS Stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_AOSP_RECOVERY := true
-ARROW_GAPPS := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := arrow_everpal
