3f20b84b2961eac82bdad4506ae14370aee78654
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2023-11-04 17:01:57 +0800

    everpal: Bringup: Welcome to Evolution X

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,29 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_everpal.mk \
-    $(LOCAL_DIR)/arrow_everpal.mk \
-    $(LOCAL_DIR)/derp_everpal.mk \
-    $(LOCAL_DIR)/evolution_everpal.mk \
-    $(LOCAL_DIR)/lineage_everpal.mk \
-    $(LOCAL_DIR)/potato_everpal.mk
+    $(LOCAL_DIR)/evolution_everpal.mk 
 
 COMMON_LUNCH_CHOICES := \
-    aosp_everpal-user \
-    aosp_everpal-userdebug \
-    aosp_everpal-eng \
-    arrow_everpal-user \
-    arrow_everpal-userdebug \
-    arrow_everpal-eng \
-    derp_everpal-user \
-    derp_everpal-userdebug \
-    derp_everpal-eng \
     evolution_everpal-user \
     evolution_everpal-userdebug \
-    evolution_everpal-eng \
-    lineage_everpal-user \
-    lineage_everpal-userdebug \
-    lineage_everpal-eng \
-    potato_everpal-user \
-    potato_everpal-userdebug \
-    potato_everpal-eng
+    evolution_everpal-eng
--- evolution_everpal.mk
+++ evolution_everpal.mk
@@ -11,9 +11,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common EvolutionX stuff.
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-# Inherit Vendor GApps
-$(call inherit-product-if-exists, vendor/gms/gms_mini.mk)
-
 # Inherit from everpal device
 $(call inherit-product, device/xiaomi/everpal/device.mk)
 
@@ -22,6 +19,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
+TARGET_BUILD_APERTURE_CAMERA := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := evolution_everpal
