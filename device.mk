979abe6f386d46a7fd742215f672aa63c0459b4f
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2023-11-17 00:35:07 +0800

    everpal: parts: Import XiaomiParts for refresh rate control
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>
    Signed-off-by: weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -501,3 +501,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
+
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
