8a1f08ed2e91c35431eb3c34578aa40d35802cf9
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2023-11-17 00:35:09 +0800

    everpal: drop IFAA
    Signed-off-by: weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -204,10 +204,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
-# IFAA
-PRODUCT_PACKAGES += \
-    IFAAService
-
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
     SystemUI
