92e65609f7b574d759a02ef864cf299b8cb55d2f
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2023-11-17 00:35:14 +0800

    everpal: copy evolution specific init file
    We are doing a13 builds so ota will work fine.
    
    Signed-off-by: weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -517,6 +517,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
 
+# Copy all Evolution X-specific init rc files
+PRODUCT_COPY_FILES += \
+    vendor/evolution/prebuilt/common/etc/init/init.evolution-x-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.evolution-x-updater.rc
+
 # XiaomiParts
 PRODUCT_PACKAGES += \
     XiaomiParts
