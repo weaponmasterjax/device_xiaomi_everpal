54d1cda51e0d2df0d77dc80a5b0298e9cfe2e814
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2023-11-17 00:34:50 +0800

    everpal: Build PowerOffAlarm
    Change-Id: I0bd11bd1f2ac465cd8820b4e24475c9576ace5a9
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -328,6 +328,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# PowerOffAlarm
+PRODUCT_PACKAGES += PowerOffAlarm
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-mediatek \
