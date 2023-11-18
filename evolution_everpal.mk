dc7dc213836081daee15fa7a409354ba0ce77970
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2023-11-18 23:14:34 +0800

    everpal: build aperture

--- evolution_everpal.mk
+++ evolution_everpal.mk
@@ -20,7 +20,7 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_ENABLE_BLUR := true
-TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_BUILD_APERTURE_CAMERA := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := evolution_everpal
