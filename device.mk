c584e48015498e07ccf0f6fda1587226aac7bf52
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2023-11-18 22:16:37 +0800

    everpal: import and mod mi_thermald configs
    Imported from evergo-missi_phoneext4_cn-user-13-TP1A.220624.014-V14.0.3.0.TGBCNXM-release-keys.
    Also dropping backlight dimming since it conflicts with aosp light hal

--- device.mk
+++ device.mk
@@ -477,6 +477,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/thermal/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-mediatekv2
