c6e93a2c959452a54c22aa5a1b0e54fbbc810577
Sushrut1101 <guptasushrut@gmail.com>
2024-02-14 19:48:40 +0500

    everpal: Import stock `power_profile`
    Change-Id: I4b0d3f5dfaac09a49a366522bf387ae57a657e1c
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, device/xiaomi/mt6833-common/mt6833.mk)
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/everpal/everpal-vendor.mk)
 
+# Overlays
+PRODUCT_PACKAGES += \
+    FrameworksResOverlayEverpal
+
 # Recovery
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/everpal/proprietary/vendor/firmware/novatek_ts_ct_fw.bin:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/novatek_ts_ct_fw.bin \
